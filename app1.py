5dd37a5bf016bfe44dcb606dc46a14f873b57b83
Matheus Gomes <matheusgomes062@gmail.com>
2020-01-18 19:36:53 -0300

    dictionary app in python done

---
+++ app1.py
@@ -0,0 +1,35 @@
+import json
+from difflib import get_close_matches
+
+data = json.load(open("data.json"))
+
+
+def translate(word):
+    word = word.lower()
+    if word in data:
+        return data[word]
+    # if user entered "texas" this will check for "Texas" as well.
+    elif word.title() in data:
+        return data[word.title()]
+    elif word.upper() in data:  # in case user enters words like USA or NATO
+        return data[word.upper()]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " %
+                   get_close_matches(word, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif yn == "N":
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "We didn't understand your entry."
+    else:
+        return "The word doesn't exist. Please double check it."
+
+
+word = input("Enter word: ")
+output = translate(word)
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
